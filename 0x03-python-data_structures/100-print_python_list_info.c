ea686e4fca66cbe4d8c62a84461c8d0ddaaa55e4
Reagan Musa <reagan.musav@gmail.com>
2023-03-13 20:28:03 +0000

    100-print_python_list_info

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <python3.4/Python.h>
+
+/**
+ * print_python_list_info - print some basic info about
+ *                          Python lists
+ * @p: object
+ *
+ * Return: void
+ *
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *listobject;
+	PyObject *element;
+	int length, i, alloc;
+
+	listobject = (PyListObject *)p;
+	length = Py_SIZE(p);
+	alloc = (listobject)->allocated;
+
+	printf("[*] Size of the python List = %d\n", length);
+	printf("[*] Allocated = %d\n", alloc);
+
+	i = 0;
+	while (i < length)
+	{
+		element = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
+		i++;
+	}
+}
