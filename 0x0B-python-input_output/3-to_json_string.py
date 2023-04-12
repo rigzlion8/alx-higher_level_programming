df27533d6a75f2943f50a87cdf60e3fbb46eaf0c
Reagan Musa <reagan.musav@gmail.com>
2023-04-12 05:58:11 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""3-to_json_string.py
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object"""
+    
+    return json.dumps(my_obj)
