6d2c53ce875fc2811c644fd655e5c064565ac14f
Reagan Musa <reagan.musav@gmail.com>
2023-04-12 03:29:41 -0700

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""2-append_write.py
+"""
+
+def append_write(filename="", text=""):
+    """Function that appends a string at the end of a text file (UTF8) and returns the no. of chars added"""
+    
+    with open(filename, 'a', encoding='utf-8') as f_obj:
+        return f.obj.write(text)
