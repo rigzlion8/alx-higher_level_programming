18374cbfdad0a0f37fb668a0bf3b9c1df56f1e20
Reagan Musa <reagan.musav@gmail.com>
2023-04-12 05:23:39 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """ function reads a text file (UTF8) and prints it to stdout """
     
-    with open(filename, 'r', encoding="utf-8") as f_obj:
-        content = f_obj.read()
-    print(content, end="")
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
