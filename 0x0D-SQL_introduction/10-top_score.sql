21718219ba216744fc2757979440bbfb8d134fb8
Reagan Musa <reagan.musav@gmail.com>
2023-06-07 16:06:51 -0700

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Create a script that lists all records of a table
+-- list all records of the table second_table of the database hbtn_0c_0
+
+SELECT
+    score, name
+FROM
+    second_table ORDER BY score DESC;
