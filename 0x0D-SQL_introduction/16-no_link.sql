80afa07d193e9b1de060b8825a994d479b5f5705
Reagan Musa <reagan.musav@gmail.com>
2023-06-07 16:29:56 -0700

    16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,11 @@
+-- Create a script that lists all records of the table second_table
+
+SELECT
+        score,
+        name
+FROM
+        second_table
+WHERE
+        name IS NOT NULL
+ORDER BY
+        score DESC;
