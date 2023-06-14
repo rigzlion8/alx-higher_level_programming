8468e1bf13b6e681c7347ec4501eedb127329d00
Reagan Musa <reagan.musav@gmail.com>
2023-06-13 18:45:17 -0700

    4-never_empty.sql

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Create a script that creates a table
+-- Create a uery to create the table 'id_not_null' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256));
