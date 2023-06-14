7a27afc0a3590c4554e5f956e21dac5459d4e012
Reagan Musa <reagan.musav@gmail.com>
2023-06-13 18:52:50 -0700

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- A script that lists all cities contained in the database
+-- A query to join cities and states
+SELECT cities.id, cities.name, states.name 
+  FROM cities
+  INNER JOIN states ON cities.state_id = states.id;
