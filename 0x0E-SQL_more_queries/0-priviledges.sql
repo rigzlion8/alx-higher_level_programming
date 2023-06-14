3b1a5907b850149871d2e99f643c87e7d5c6ed08
Reagan Musa <reagan.musav@gmail.com>
2023-06-13 18:37:30 -0700

    0-priviledges.sql

---
+++ 0x0E-SQL_more_queries/0-priviledges.sql
@@ -0,0 +1,4 @@
+-- Create a script that lists all privileges of the MySQL users
+-- Also, a query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
