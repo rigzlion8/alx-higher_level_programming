672e48d49a3d210c9639675dfc9e6d49eefc73f1
Reagan Musa <reagan.musav@gmail.com>
2023-07-04 05:17:14 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print("Body response:")
+    print(f"\t- type: {type(text)}")
+    print(f"\t- content: {text}")
